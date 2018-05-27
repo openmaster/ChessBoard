0908823ff5171ba659dff5e4ab9625dd5d41e525
Vishavjeet Singh <vishav4@gmail.com>
2018-05-27 03:08:15 -0400

    UI implementation through directive is done

--- directives/chess-board.js
+++ directives/chess-board.js
@@ -8,9 +8,78 @@ chessApp.directive('chessBoard', function() {
 		  chess_board: "=dimentions"
 		},
 		link: function(scope, element, attrs) {
+			scope.chess = [];
 			scope.show = function(){
 				console.log(scope.chess_board);
 			}
+
+			push_black = function(i, j){
+				return {
+					class:'black',
+					i: i,
+					j: j
+				};
+			}
+
+			push_white = function(i, j){
+				return{
+					class:'white',
+					i: i,
+					j: j
+				};
+			}
+
+			starting_white = function(dim, i){
+				node = [];
+				for(j = 1; j <= dim.cols; j++){
+					if(j%2 == 0){
+						//even coloumn
+						node.push(push_black(i, j));
+					} else {
+						node.push(push_white(i, j));
+					}
+				}
+				scope.chess.push(node);
+			}
+
+			starting_black = function(dim, i){
+				node = [];
+				for(j = 1; j <= dim.cols; j++){
+					if(j%2 == 0){
+						//even coloumn
+						node.push(push_white(i, j));
+					} else {
+						node.push(push_black(i, j));
+					}
+				}
+				scope.chess.push(node);
+			}
+
+			create_chess = function(dim) {
+				//dim object should have rows and cols
+				scope.chess = [];
+				if(dim.rows != null && dim.cols != null) {
+					console.log('no problem');
+					for(i = 1; i <= dim.rows; i++) {
+						//adding number of rows
+						if(i%2 == 0){
+							//this is even row
+							starting_white(dim, i);
+						} else {
+							//this is odd row
+							starting_black(dim, i);
+						}
+					}
+					console.log(scope.chess);
+				}
+			}
+
+			scope.$watch('chess_board', function(values){
+				if(values){
+					console.log(values);
+					create_chess(values);
+				}
+			}, true);
 
 		}
 	};
